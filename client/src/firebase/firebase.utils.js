d7c6115cf1ec67d32274fa55e3424125758b661a
CHETAN KUMAR SINHA <83967941+ChetanKumarSinha@users.noreply.github.com>
2021-12-23 22:14:57 +0530

    Update firebase.utils.js

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: "AIzaSyA2yiiKH_nioMJde3RKHVyEXcrEASRI7lE",
-  authDomain: "crwn-db-cd4c2.firebaseapp.com",
-  projectId: "crwn-db-cd4c2",
-  storageBucket: "crwn-db-cd4c2.appspot.com",
-  messagingSenderId: "642660704716",
-  appId: "1:642660704716:web:5345f4b2d6b9b253b2fd69",
-  measurementId: "G-74R3R8JVJ8"
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLGOv_kcTEyTRHXA62v07MTuD7xS7Asc",
+  authDomain: "urban-clothing-f4487.firebaseapp.com",
+  projectId: "urban-clothing-f4487",
+  storageBucket: "urban-clothing-f4487.appspot.com",
+  messagingSenderId: "418717518854",
+  appId: "1:418717518854:web:148e74a7f6554e88a14787",
+  measurementId: "G-PG81N2E16X"
 };
 
 firebase.initializeApp(config);
